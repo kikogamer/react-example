ad4d23a1f9eab63ae3b75852f5c72f1a4a9f2528
Ronaldo <kikogamer@gmail.com>
2019-05-09 00:20:54 -0300

    component properties

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import News from './news';
+import News from './news/list';
 
-ReactDOM.render(<News/>, document.getElementById('app'));
+ReactDOM.render(<News title="Heyyyy"/>, document.getElementById('app'));
